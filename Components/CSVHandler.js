72e6da663ba2b3923dae92200bfc612dc6b60c46
Tulaib <tulaib.pirzada@ebricks-inc.com>
2018-11-26 17:48:59 +0500

    Add read csv functionality

--- Components/CSVHandler.js
+++ Components/CSVHandler.js
@@ -1,19 +1,19 @@
 var csv = require("fast-csv");
 var fs  = require('fs');
+const util = require('util');
 
 const fileName = 'data.csv'
 const filepath = fileName
 var csvStream = csv.format({headers: true}); 
-var writableStream = fs.createWriteStream(filepath);
 
 const writeData = (dataList) => {
+    var writableStream = fs.createWriteStream(filepath);
     writableStream.on("finish", () => {
         console.log("DONE!");
     });
 
     csvStream.pipe(writableStream);
-    var i;
-    for (i = 0; i < dataList.length; i++) {
+    for (var i = 0; i < dataList.length; i++) {
         const user = dataList[i];
         csvStream.write({FirstName: user['firstName'], LastName: user['lastName'], 
             Country: user['country'], CreatedAt: user['createdAt'], Username: user['username']});
@@ -21,6 +21,20 @@ const writeData = (dataList) => {
     csvStream.end();
 }
 
+const readData = () => {
+    var readStream = fs.createReadStream(filepath);
+ 
+    csv
+    .fromStream(readStream, {headers : true})
+    .on("data", (data) => {
+        console.log(data);
+    })
+    .on("end", () => {
+        console.log("done");
+    });
+}
+
 module.exports = {
-    writeData
+    writeData,
+    readData
 }
--- server.js
+++ server.js
@@ -3,29 +3,12 @@ const util = require('util');
 
 const csvHandler = require('./Components/CSVHandler')
 
-var user = {
-    firstName: {
-        faker: 'name.firstName'
-    },
-    lastName: {
-        faker: 'name.lastName'
-    },
-    country: {
-        faker: 'address.country'
-    },
-    createdAt: {
-        faker: 'date.past'
-    },
-    username: {
-        function: function() {
-            return (
-                this.object.lastName.substring(0, 5) +
-                this.object.firstName.substring(0, 3) +
-                Math.floor(Math.random() * 10)
-            )
-        }
-    }
+function mockData() {
+    // createMockData()
+    readMockData()
 }
+
+mockData()
 // var group = {
 //     description: {
 //         faker: 'lorem.paragraph'
@@ -52,17 +35,46 @@ var user = {
 //     }
 // }
 
-mocker()
+function readMockData() {
+    csvHandler.readData();
+}
+
+function createMockData() {
+    var user = {
+        firstName: {
+            faker: 'name.firstName'
+        },
+        lastName: {
+            faker: 'name.lastName'
+        },
+        country: {
+            faker: 'address.country'
+        },
+        createdAt: {
+            faker: 'date.past'
+        },
+        username: {
+            function: function() {
+                return (
+                    this.object.lastName.substring(0, 5) +
+                    this.object.firstName.substring(0, 3) +
+                    Math.floor(Math.random() * 10)
+                )
+            }
+        }
+    }
+
+    mocker()
     .schema('user', user, 5)
     // .schema('group', group, 5)
     // .schema('conditionalField', conditionalField, 5)
     .build()
     .then(
         data => {
-            // console.log(util.inspect(data, { depth: 10 }));
             const userList = data['user'];
             console.log(userList.length);
             csvHandler.writeData(userList)
         },
         err => console.error(err)
     )
+}
